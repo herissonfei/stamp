9d080e2ba965aa1b913218e2510594bfdee3a224
Hangfei Lin <1023584582@qq.com>
2024-01-01 21:31:35 -0500

    modification

--- resources/data/cv.sql
+++ resources/data/cv.sql
@@ -1,27 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- 主机： 127.0.0.1:3307
--- 生成日期： 2023-11-01 04:09:16
--- 服务器版本： 10.10.2-MariaDB
--- PHP 版本： 8.2.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- 数据库： `cv`
---
-
--- --------------------------------------------------------
 
 --
 -- 表的结构 `bidhistory`
@@ -287,6 +266,4 @@ CREATE TABLE IF NOT EXISTS `watchlist` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf32 COLLATE=utf32_general_ci;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
